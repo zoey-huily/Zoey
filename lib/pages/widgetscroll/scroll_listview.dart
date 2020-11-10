14ac6c88d96829378b4c016b9ada93ba6837c928
zhuyuexiang <1455794626@qq.com>
2020-11-10 10:58:14 +0800

    no message

--- lib/pages/widgetscroll/scroll_listview.dart
+++ lib/pages/widgetscroll/scroll_listview.dart
@@ -9,11 +9,23 @@ class ScrollListView extends StatelessWidget{
         title: Text("ListView"),
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[
-
-        ],
-      )
+      // body: ListView(
+      //   shrinkWrap: true,
+      //   padding: const EdgeInsets.all(20.0),
+      //   children: <Widget>[
+      //     const Text('I\'m dedicating every day to you'),
+      //     const Text('Domestic life was never quite my style'),
+      //     const Text('When you smile, you knock me out, I fall apart'),
+      //     const Text('And I thought I was so smart'),
+      //   ],
+      // )
+        body:ListView.builder(
+            itemCount: 100,
+            itemExtent: 50.0, //强制高度为50.0
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(title: Text("$index"));
+            }
+        )
 
     );
   }
--- lib/pages/widgetscroll/scroll_single_child.dart
+++ lib/pages/widgetscroll/scroll_single_child.dart
@@ -3,18 +3,46 @@ import 'package:flutter/material.dart';
 class ScrollSingleChild extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text("SingleChildScrollView"),
-        centerTitle: true,
+    String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return Scrollbar( // 显示进度条
+      child: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            //动态创建一个List<Widget>
+            children: str.split("")
+            //每一个字母都用一个Text显示,字体为原来的两倍
+                .map((c) => Text(c, textScaleFactor: 2.0,))
+                .toList(),
+          ),
+        ),
       ),
-      body: Column(
-        children: <Widget>[
-
-        ],
-      )
-
     );
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     automaticallyImplyLeading: false,
+    //     title: Text("SingleChildScrollView"),
+    //     centerTitle: true,
+    //   ),
+    //   body: Column(
+    //     children: <Widget>[
+    //         Scrollbar( // 显示进度条
+    //           child: SingleChildScrollView(
+    //           padding: EdgeInsets.all(16.0),
+    //           child: Center(
+    //             child: Column(
+    //               //动态创建一个List<Widget>
+    //               children: str.split("")
+    //               //每一个字母都用一个Text显示,字体为原来的两倍
+    //                   .map((c) => Text(c, textScaleFactor: 2.0,))
+    //                   .toList(),
+    //             ),
+    //           ),
+    //         ),
+    //       )
+    //     ],
+    //   )
+    //
+    // );
   }
 }
